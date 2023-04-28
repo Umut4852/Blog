acdfac950b66fcf2ba7e4078cd5f4b24ca4dc716
Umut_ <Umut_2264@outlook.com>
2023-04-28 15:03:45 +0300

    1

--- Blog_Data/Context/AppDbContext.cs
+++ Blog_Data/Context/AppDbContext.cs
@@ -1,12 +1,6 @@
-﻿using Bogus.DataSets;
+﻿using Blog_Entity.Entities;
+using Bogus.DataSets;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ZendeskApi_v2.Models.Articles;
-using ZendeskApi_v2.Models.HelpCenter.Categories;
 
 namespace Blog_Data.Context
 {
